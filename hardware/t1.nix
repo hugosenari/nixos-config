afc38b37aef0eee9d620af7735de957547d1cf2c
hugosenari <hugosenari@gmail.com>
2022-08-21 01:27:32 -0300

    chore(T1): hardware

--- hardware/t1.nix
+++ hardware/t1.nix
@@ -1,23 +1,35 @@
+# One Netbook T1
+
 { config, lib, pkgs, modulesPath, ... }:
 {
-  imports     = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+  imports     = [ "${modulesPath}/installer/scan/not-detected.nix" ];
   swapDevices = [ ];
-
-  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usbhid" "usb_storage" "sd_mod" ];
-  boot.extraModulePackages  = [ ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules        = [ "kvm-intel" ];
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint     = "/boot/efi";
-  boot.loader.systemd-boot.enable      = true;
-
-  fileSystems."/".device         = "/dev/disk/by-uuid/88ca8bba-0fc3-4fe8-bb7e-6e2548da988d";
-  fileSystems."/".fsType         = "ext4";
-  fileSystems."/boot/efi".device = "/dev/disk/by-uuid/9EC7-0DEC";
-  fileSystems."/boot/efi".fsType = "vfat";
 
   networking.hostName = "T1";
   networking.useDHCP  = lib.mkDefault true;
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  boot.extraModulePackages  = [ ];
+  boot.kernelModules        = [ "kvm-intel" ];
+  boot.initrd.availableKernelModules   = [ "xhci_pci" "thunderbolt" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules            = [ "i915" ];
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint     = "/boot/efi";
+  boot.loader.systemd-boot.enable      = true;
+  fileSystems."/".device         = "/dev/nvme0n1p4";
+  fileSystems."/".fsType         = "ext4";
+  fileSystems."/boot/efi".device = "/dev/nvme0n1p1";
+  fileSystems."/boot/efi".fsType = "vfat";
+
+  environment.variables.VDPAU_DRIVER = "va_gl";
+  powerManagement.cpuFreqGovernor    = "powersave";
+  hardware.cpu.intel.updateMicrocode = config.hardware.enableRedistributableFirmware;
+  hardware.opengl.enable             = true;
+  hardware.opengl.extraPackages      = with pkgs; [vaapiIntel libvdpau-va-gl intel-media-driver];
+  hardware.sensor.iio.enable         = true;
+
+  services.udev.extraHwdb = ''
+    acpi:BOSC0200:BOSC0200:*
+     ACCEL_MOUNT_MATRIX=0, 1, 0; 0, 0, 1; 1, 0, 0
+  '';
+
 }
