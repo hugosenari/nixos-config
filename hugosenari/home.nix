e7f1dd3909526e2aeeb3e055c959dcffbbe300f2
hugosenari <hugosenari@gmail.com>
2022-08-23 18:05:03 -0300

    chore: sort

--- hugosenari/home.nix
+++ hugosenari/home.nix
@@ -8,23 +8,18 @@ in
   home.packages      = [
     # (pkgs.callPackage (sources.funcoeszz + "/default.nix") {})
     # (pkgs.callPackage (sources.gmusicbrowser-nixpkgx + "/default.nix") {})
-    # pkgs.slack
-    # pkgs.vlc
-    # pkgs.zoom-us
     pkgs.chromium
     pkgs.keybase-gui
+    pkgs.openconnect
     pkgs.tdesktop
     pkgs.teams
-    pkgs.networkmanagerapplet
-    pkgs.openconnect
     pkgs.xclip
   ]; 
   home.stateVersion  = "22.05";
   home.username      = "hugosenari";
-  home.sessionVariables.EDITOR = "vim";
 
-  nixpkgs.config.allowUnfree = true;
-
+  home.sessionVariables.EDITOR       = "vim";
+  nixpkgs.config.allowUnfree         = true;
   programs.command-not-found.enable  = false;
   programs.direnv.enable             = true;
   programs.fish.enable               = true;
@@ -35,9 +30,8 @@ in
   programs.jq.enable                 = true;
   programs.mpv.enable                = true;
   programs.password-store.enable     = true;
-
-  services.kbfs.enable    = true;
-  services.keybase.enable = true;
+  services.kbfs.enable               = true;
+  services.keybase.enable            = true;
 
   xdg.enable = true;
 }
