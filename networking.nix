9253ec0f0b56eac4858528561b773a6594a1206f
hugosenari <hugosenari@gmail.com>
2022-10-10 16:08:12 -0300

    feat: new host

--- networking.nix
+++ networking.nix
@@ -1,9 +1,9 @@
 {
   networking.enableIPv6 = false;
   networking.firewall.enable       = false;
-  networking.hosts."0.0.0.0"       = [ "postgres" "coltrane-api" ];
   networking.hosts."69.74.69.80"   = [ "dev.live.cdn.optimum.net" ];
   networking.hosts."192.168.0.163" = [ "hp.lilasp" ];
   networking.hosts."192.168.0.167" = [ "t1.lilasp" ];
+  networking.hosts."192.168.0.192" = [ "bo.lilasp" ];
   networking.hosts."192.168.0.72"  = [ "mi.lilasp" ];
 }
