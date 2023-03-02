a9afec69e2a4f1fed762c2145054bf0e0ffe64bb
hugosenari <hugosenari@gmail.com>
2023-03-02 04:11:32 -0300

    feat: add unfree

--- cfg.nix
+++ cfg.nix
@@ -49,7 +49,11 @@
 
   nix.extraOptions = "experimental-features = nix-command flakes";
   nix.package      = pkgs.nixFlakes;
-  nix.settings.trusted-users = [ "root" "hugosenari" ];
+  nix.settings.auto-optimise-store = true;
+  nix.settings.substituters        = [ "https://nix-community.cachix.org" "https://numtide.cachix.org"];
+  nix.settings.trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="];
+  nix.settings.trusted-users       = [ "root" "hugosenari" ];
+
   security.rtkit.enable        = true;
   services.acpid.enable        = true;
   services.cachix-agent.enable = true;
--- flake.nix
+++ flake.nix
@@ -2,8 +2,10 @@
   description             = "Hugosenari Hosts";
   #inputs.nixpkgs.url     = "github:NixOs/nixpkgs";  # unstable branch
   inputs.nixpkgs.url      = "github:NixOs/nixpkgs/release-22.11";
+  inputs.unfree.url       = "github:numtide/nixpkgs-unfree";
   inputs.home-manager.url = "github:nix-community/home-manager/master";
   inputs.cd.url           = "github:cachix/cachix-deploy-flake";
+  # inputs.unfree.inputs.nixpkgs.follows       = "nixpkgs";
   inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs = inputs:
@@ -18,18 +20,13 @@
       modules     = [ module ];
       specialArgs = { inherit inputs; };
     };
-    mkHM   = modules: inputs.home-manager.lib.homeManagerConfiguration {
-      inherit modules pkgs;
-    };
     cd-lib = inputs.cd.lib pkgs;
     cd     = cd-lib.spec {
-      agents.T1 = cd-lib.nixos           inputs.self.nixosModules.T1;
-      agents.HP = cd-lib.nixos           inputs.self.nixosModules.HP;
-      agents.BO = cd-lib.homeManager { } inputs.self.homeModules.BO;
+      agents.T1 = cd-lib.nixos inputs.self.nixosModules.T1;
+      agents.HP = cd-lib.nixos inputs.self.nixosModules.HP;
+      agents.BO = cd-lib.nixos inputs.self.nixosModules.BO;
     };
   in {
-    homeConfigurations."hugo.s.ribeiro@wpteng279"  = mkHM [ inputs.self.homeModules.BO ];
-    homeModules.BO.imports   = [ ./hugo.s.ribeiro/home-manager.nix ];
     nixosConfigurations.BO   = mkOS inputs.self.nixosModules.BO;
     nixosConfigurations.HP   = mkOS inputs.self.nixosModules.HP;
     nixosConfigurations.T1   = mkOS inputs.self.nixosModules.T1;
