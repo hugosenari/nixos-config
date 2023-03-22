b3c473124467a6b78bf07dfb731daa6801c77ca2
hugosenari <hugosenari@gmail.com>
2023-03-21 21:26:44 -0300

    feat: use secrets from keybase

--- hugo/home-manager.nix
+++ hugo/home-manager.nix
@@ -15,8 +15,13 @@ in
     pkgs.xclip
   ]; 
   home.sessionVariables.EDITOR       = "vim";
+  nix.extraOptions = ''
+    !include ${config.home.homeDirectory}/keybase/private/hugosenari/nix.conf
+  '';
+
   nixpkgs.config.allowUnfree         = true;
   nixpkgs.config.allowUnfreePredicate= (pkg: true);
+
   programs.command-not-found.enable  = false;
   programs.direnv.enable             = true;
   programs.fish.enable               = true;
@@ -27,6 +32,18 @@ in
   programs.jq.enable                 = true;
   programs.mpv.enable                = true;
   programs.password-store.enable     = true;
+  programs.ssh.enable                = true;
+  programs.ssh.compression           = true;
+  programs.ssh.controlMaster         = "auto";
+  programs.ssh.controlPath           = "/tmp/ssh-%r@%h:%p";
+  programs.ssh.controlPersist        = "1h";
+  programs.ssh.extraConfig           = ''
+    IdentityFile %h/keybase/private/hugosenari/ssh/id_rsa
+    IdentityFile %h/keybase/private/hugosenari/ssh/id_rsa_altice
+    IdentityFile %h/keybase/private/hugosenari/ssh/id_ed25519
+    IdentityFile %h/keybase/private/hugosenari/ssh/google_compute_engine
+  '';
+
   services.kbfs.enable               = true;
   services.keybase.enable            = true;
   xdg.enable = true;
