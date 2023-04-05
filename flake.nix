08045bcb7be5a134a8c1f22d7f427e98ad85a8d9
hugosenari <hugosenari@gmail.com>
2023-04-05 10:54:14 -0300

    chore: simplify flake file

--- flake.nix
+++ flake.nix
@@ -1,30 +1,28 @@
 {
-  description             = "Hugosenari Hosts";
-  #inputs.nixpkgs.url     = "github:NixOs/nixpkgs";  # unstable branch
-  inputs.nixpkgs.url      = "github:NixOs/nixpkgs/release-22.11";
-  inputs.unfree.url       = "github:numtide/nixpkgs-unfree";
-  inputs.home-manager.url = "github:nix-community/home-manager/master";
-  inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  description        = "Hugosenari Hosts";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
+  inputs.unfree.url  = "github:numtide/nixpkgs-unfree";
+  inputs.hm.url      = "github:nix-community/home-manager/master";
+  inputs.hm.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = inputs:
+  nixConfig.extra-substituters        = [ "https://numtide.cachix.org" ];
+  nixConfig.extra-trusted-public-keys = [ "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE=" ];
+
+  outputs = {self, hm, ...}@inputs:
   let
-    system = "x86_64-linux";
-    pkgs   = import "${inputs.nixpkgs}" {
-      inherit system;
-      config.allowUnfree = true;
-    };
-    mkOS   = module: inputs.nixpkgs.lib.nixosSystem {
-      inherit system;
+    mkOS = module: inputs.nixpkgs.lib.nixosSystem {
       modules     = [ module ];
-      specialArgs = { inherit inputs; };
+      system      = "x86_64-linux";
+      specialArgs.inputs = inputs;
+      specialArgs.unfree = inputs.unfree.x86_64-linux;
     };
   in {
-    nixosConfigurations.BO   = mkOS inputs.self.nixosModules.BO;
-    nixosConfigurations.HP   = mkOS inputs.self.nixosModules.HP;
-    nixosConfigurations.T1   = mkOS inputs.self.nixosModules.T1;
-    nixosModules.BO.imports  = [ inputs.self.nixosModules.cfg ./bo ];
-    nixosModules.HP.imports  = [ inputs.self.nixosModules.cfg ./hp ];
-    nixosModules.T1.imports  = [ inputs.self.nixosModules.cfg ./t1 ];
-    nixosModules.cfg.imports = [ inputs.home-manager.nixosModules.home-manager ./cfg.nix ./hugosenari ./networking.nix ];
+    nixosConfigurations.BO   = mkOS self.nixosModules.BO;
+    nixosConfigurations.HP   = mkOS self.nixosModules.HP;
+    nixosConfigurations.T1   = mkOS self.nixosModules.T1;
+    nixosModules.ALL.imports = [ hm.nixosModules.home-manager ./cfg.nix ./hugosenari ./networking.nix ];
+    nixosModules.BO.imports  = [ self.nixosModules.ALL ./bo ];
+    nixosModules.HP.imports  = [ self.nixosModules.ALL ./hp ];
+    nixosModules.T1.imports  = [ self.nixosModules.ALL ./t1 ];
   };
 }
