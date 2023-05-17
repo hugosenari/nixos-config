ae870eec1a228d0e9109a3739fc7df881d07208c
Hugo Sena Ribeiro <hugosenari@gmail.com>
2023-05-17 12:31:39 -0300

    Update cfg.nix

--- cfg.nix
+++ cfg.nix
@@ -62,7 +62,7 @@
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
   ];
-  nix.settings.post-build-hook     = pkgs.writeShellScriptBin "my_own_cache_with_blackjack_and_hookers" ''
+  nix.settings.post-build-hook     = pkgs.writeShellScript "my_own_cache_with_blackjack_and_hookers" ''
     echo "Uploading paths" $OUT_PATHS
     nix store sign --key-file /etc/nix/nixstore-key $OUT_PATHS
     exec nix copy --to 's3://nixstore?profile=nixstore&endpoint=q4n8.or.idrivee2-24.com' $OUT_PATHS
