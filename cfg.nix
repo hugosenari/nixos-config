64a10a28957959b61a3cdb994f5e711734d7912d
Hugo Sena Ribeiro <hugosenari@gmail.com>
2023-05-17 02:37:45 -0300

    Update cfg.nix

--- cfg.nix
+++ cfg.nix
@@ -82,6 +82,8 @@
   system.autoUpgrade.dates  = "2 h";
   system.autoUpgrade.flake  = "github:hugosenari/nixos-config#${config.networking.hostName}";
   system.autoUpgrade.randomizedDelaySec = "5m";
-
+  
+  systemd.services.nix-daemon.serviceConfig.Environment.AWS_CREDENTIAL_PROFILES_FILE = "/var/aws/credentials";
+  
   virtualisation.docker.enable = true;
 }
