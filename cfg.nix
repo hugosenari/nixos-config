c1bb7d2339e83455097458f89e2c013598fc22bd
Hugo Sena Ribeiro <hugosenari@gmail.com>
2023-05-17 15:34:14 -0300

    Update cfg.nix

--- cfg.nix
+++ cfg.nix
@@ -53,7 +53,7 @@
   nix.gc.randomizedDelaySec = "45min";
   nix.settings.auto-optimise-store = true;
   nix.settings.substituters        = [
-    "s3://nixstore?profile=nixstore&endpoint=q4n8.or.idrivee2-24.com"
+    "s3://nixstore?compression=zstd&profile=nixstore&endpoint=q4n8.or.idrivee2-24.com"
     "https://nix-community.cachix.org"
     "https://numtide.cachix.org"
   ];
@@ -64,13 +64,15 @@
   ];
   nix.settings.post-build-hook     = pkgs.writeShellScript "myOwnCacheWithBlackjackAndHooks" ''
     nix store sign --key-file /etc/nix/nixstore-key $OUT_PATHS
-    export AWS_CREDENTIAL_PROFILES_FILE=/etc/aws/credentials
+    echo "AWS_SHARED_CREDENTIALS_FILE $AWS_SHARED_CREDENTIALS_FILE" >> /tmp/nix-build-logs.txt
+    export AWS_SHARED_CREDENTIALS_FILE=/etc/aws/credentials
+    echo "AWS_SHARED_CREDENTIALS_FILE $AWS_SHARED_CREDENTIALS_FILE" >> /tmp/nix-build-logs.txt
     exec nix copy \
-      --to 's3://nixstore?profile=nixstore&endpoint=q4n8.or.idrivee2-24.com' \
+      --to 's3://nixstore?compression=zstd&profile=nixstore&endpoint=q4n8.or.idrivee2-24.com' \
       $OUT_PATHS &
   '';
-  systemd.services.nix-daemon.serviceConfig.Environment = ''"AWS_CREDENTIAL_PROFILES_FILE=/etc/aws/credentials"'';
-  nix.settings.trusted-users       = [ "root" "hugosenari" ];
+  systemd.services.nix-daemon.serviceConfig.Environment = ''"AWS_SHARED_CREDENTIALS_FILE=/etc/aws/credentials"'';
+  nix.settings.trusted-users       = [ "root" "hugosenari" "@nixbld"];
 
   security.rtkit.enable        = true;
   services.acpid.enable        = true;
