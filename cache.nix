57762868b4c2c360766580a1fde60d31b3f443cb
hugosenari <hugosenari@gmail.com>
2023-05-23 14:07:55 -0300

    enable content addressed

--- cache.nix
+++ cache.nix
@@ -4,14 +4,12 @@
   nix.gc.randomizedDelaySec        = "45min";
   nix.settings.auto-optimise-store = true;
   nix.settings.substituters        = [
-    "s3://nixstore?compression=zstd&profile=nixstore&endpoint=q4n8.or.idrivee2-24.com"
-    "https://nix-community.cachix.org"
-    "https://numtide.cachix.org"
+    "s3://nixstore?compression=zstd&profile=nixstore&endpoint=q4n8.or.idrivee2-24.com" # My Cache
+    "https://cache.ngi0.nixos.org/"                                                    # Content Addressed
   ];
   nix.settings.trusted-public-keys = [ 
-    "nixstore:XPnWsxFA3W5WV9nl6TFA1EhYkehVMIZOs20wuAf8A5c="
-    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+    "nixstore:XPnWsxFA3W5WV9nl6TFA1EhYkehVMIZOs20wuAf8A5c="                            # My Cache
+    "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA="              # Content Addressed
   ];
   nix.settings.post-build-hook     = pkgs.writeShellScript "myOwnCacheWithBlackjackAndHooks" ''
     nix store sign --key-file /etc/nix/nixstore-key $OUT_PATHS
