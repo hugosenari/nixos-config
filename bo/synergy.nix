4bd2255b13480b4712387f75baa5870b0d364e20
hugosenari <hugosenari@gmail.com>
2023-05-25 00:50:47 -0300

    change synergy server

--- bo/synergy.nix
+++ bo/synergy.nix
@@ -1,6 +1,36 @@
 {
-  services.synergy.client.autoStart     = true;
-  services.synergy.client.enable        = true;
-  services.synergy.client.screenName    = "bo";
-  services.synergy.client.serverAddress = "hp.lilasp";
+  services.synergy.server.address    = "bo.lilasp";
+  services.synergy.server.autoStart  = true;
+  services.synergy.server.enable     = true;
+  services.synergy.server.screenName = "bo";
+  services.synergy.server.configFile = builtins.toFile "synergyCfg" ''
+    section: screens
+      t1:
+      hp:
+      bo:
+    end
+    section: aliases
+        t1:
+          t1.lilasp
+        hp:
+          hp.lilasp
+        bo:
+          bo.lilasp
+    end
+    section: links
+       t1:
+           left  = hp
+           up    = bo
+       hp:
+           right = t1
+           up    = bo
+       bo:
+           left  = hp
+           down  = t1
+    end
+  '';
+
+  # t1.lilasp and hp.lilasp are configured in networking.nix in 'networking.hosts. ...'
 }
+
+
--- t1/synergy.nix
+++ t1/synergy.nix
@@ -2,6 +2,6 @@
   services.synergy.client.autoStart     = true;
   services.synergy.client.enable        = true;
   services.synergy.client.screenName    = "t1";
-  services.synergy.client.serverAddress = "hp.lilasp";
+  services.synergy.client.serverAddress = "bo.lilasp";
   # t1.lilasp and hp.lilasp are configured in networking.nix in 'networking.hosts. ...'
 }
