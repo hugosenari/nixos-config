5962c862b293ddf97ee011dae37cd7aecd352cba
hugosenari <hugosenari@gmail.com>
2023-05-27 07:03:42 -0300

    remove unsused cache providers

--- cache.nix
+++ cache.nix
@@ -4,19 +4,18 @@
   nix.gc.randomizedDelaySec        = "45min";
   nix.settings.auto-optimise-store = true;
   nix.settings.substituters        = [
-    "s3://nixstore?compression=zstd&profile=nixstore&endpoint=q4n8.or.idrivee2-24.com" # My Cache
-    "https://cache.ngi0.nixos.org/"                                                    # Content Addressed
+    "s3://nixstore?compression=zstd&profile=nixstore&endpoint=q4n8.or.idrivee2-24.com" # My Cache Private S3
   ];
   nix.settings.trusted-public-keys = [ 
-    "nixstore:XPnWsxFA3W5WV9nl6TFA1EhYkehVMIZOs20wuAf8A5c="                            # My Cache
-    "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA="              # Content Addressed
+    "nixstore:XPnWsxFA3W5WV9nl6TFA1EhYkehVMIZOs20wuAf8A5c="                            # My Cache Public Key
   ];
   nix.settings.post-build-hook     = pkgs.writeShellScript "myOwnCacheWithBlackjackAndHooks" ''
-    nix store sign --key-file /etc/nix/nixstore-key $OUT_PATHS
+    NIXSIGN_KEY=/etc/nix/nixstore-key                                                  # My Cache Private Key
+    nix store sign --key-file $NIXSIGN_KEY $OUT_PATHS
     export AWS_SHARED_CREDENTIALS_FILE=/etc/aws/credentials
-    exec nix copy \
+    nix copy \
       --to 's3://nixstore?compression=zstd&profile=nixstore&endpoint=q4n8.or.idrivee2-24.com' \
-      $OUT_PATHS
+      $OUT_PATHS &
   '';
   systemd.services.nix-daemon.serviceConfig.Environment = ''"AWS_SHARED_CREDENTIALS_FILE=/etc/aws/credentials"'';
 }
