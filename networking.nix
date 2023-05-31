aa826598e2d02692abef2cbc189fe1a9a0842c4b
hugosenari <hugosenari@gmail.com>
2023-05-31 19:14:44 -0300

    fix: remove work specific ip

--- networking.nix
+++ networking.nix
@@ -1,7 +1,6 @@
 {
   networking.enableIPv6 = false;
   networking.firewall.enable       = false;
-  networking.hosts."69.74.69.80"   = [ "dev.live.cdn.optimum.net" ];
   networking.hosts."192.168.0.163" = [ "hp.lilasp" ];
   networking.hosts."192.168.0.167" = [ "t1.lilasp" ];
   networking.hosts."192.168.0.192" = [ "bo.lilasp" ];
