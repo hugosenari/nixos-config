8938f26cf96674f460470e49ee4995a1b90a5286
hugosenari <hugosenari@gmail.com>
2023-06-20 18:15:43 -0300

    feat: lib

--- flake.nix
+++ flake.nix
@@ -6,18 +6,21 @@
   inputs.hm.url      = "github:nix-community/home-manager/master";
   inputs.hm.inputs.nixpkgs.follows = "nixpkgs";
 
+
   outputs = inputs: rec {
     lib = import ./lib.nix inputs "x86_64-linux";
 
-    homeConfigurations.hugo = lib.home homeModules.hsr;
-    homeModules.hsr.imports = [ ./hugosenari/home-manager.nix ];
+    homeModules.eu.imports  = [ ./hugosenari/home-manager.nix ];
+    homeConfigurations.eu   = lib.home homeModules.eu;
+
+    nixosModules.eu.imports = [ ./hugosenari ];
+    nixosModules.os.imports = [ ./cfg.nix ./cache.nix ./networking.nix ];
+    nixosModules.BO.imports = [ nixosModules.os  nixosModules.eu  ./bo ];
+    nixosModules.HP.imports = [ nixosModules.os  nixosModules.eu  ./hp ];
+    nixosModules.T1.imports = [ nixosModules.os  nixosModules.eu  ./t1 ];
 
     nixosConfigurations.BO  = lib.os nixosModules.BO;
     nixosConfigurations.HP  = lib.os nixosModules.HP;
     nixosConfigurations.T1  = lib.os nixosModules.T1;
-    nixosModules.BO.imports = [ nixosModules.__ ./bo ];
-    nixosModules.HP.imports = [ nixosModules.__ ./hp ];
-    nixosModules.T1.imports = [ nixosModules.__ ./t1 ];
-    nixosModules.__.imports = [ ./cfg.nix ./cache.nix ./hugosenari ./networking.nix ];
   };
 }
