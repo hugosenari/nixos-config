be801dd162a143baa16bd93b460eb71be2762997
hugosenari <hugosenari@gmail.com>
2023-07-12 23:07:31 -0300

    chore: change self refname

--- hugosenari/default.nix
+++ hugosenari/default.nix
@@ -1,7 +1,7 @@
 { pkgs, inputs, ... }: {
   home-manager.useGlobalPkgs          = true;
   home-manager.useUserPackages        = true;
-  home-manager.users.hugosenari       = inputs.self.homeModules.eu;
+  home-manager.users.hugosenari       = inputs.self.homeModules.I;
   users.users.hugosenari.description  = "hugosenari";
   users.users.hugosenari.extraGroups  = [ "networkmanager" "wheel" "sudo" "lp" "docker" ];
   users.users.hugosenari.isNormalUser = true;
