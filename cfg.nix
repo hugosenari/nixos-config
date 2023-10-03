423d73b72de3502efed768b532f4a4c5e13c88c5
hugosenari <hugosenari@gmail.com>
2023-10-03 20:15:59 -0300

    chore: weekly gc

--- cfg.nix
+++ cfg.nix
@@ -50,6 +50,7 @@
   nix.package      = pkgs.nixVersions.nix_2_15;
   nix.gc.automatic                 = true;
   nix.gc.randomizedDelaySec        = "46min";
+  nix.gc.dates                     = "weekly";
   nix.settings.auto-optimise-store = true;
   nix.settings.substituters        = [ "https://numtide.cachix.org" ];
   nix.settings.trusted-public-keys = [ "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE=" ];
