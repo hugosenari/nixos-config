001a4b135705e9c6e856d0b27c57e163b2702304
hugosenari <hugosenari@gmail.com>
2024-02-27 18:39:38 -0300

    desnscd

--- networking.nix
+++ networking.nix
@@ -1,5 +1,7 @@
+{lib, ...}:
 {
   services.nscd.enable  = false;
+  system.nssModules = lib.mkForce [];
   networking.enableIPv6 = false;
   networking.firewall.enable       = false;
   networking.hosts."192.168.0.163" = [ "hp.lilasp" ];
