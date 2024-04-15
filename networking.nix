9304965f82a6bda04bbaea530c3e9c1fb744c42d
hugosenari <hugosenari@gmail.com>
2024-04-15 19:44:31 -0300

    networking: append hosts from home

--- networking.nix
+++ networking.nix
@@ -1,4 +1,6 @@
-{lib, ...}:
+{lib, pkgs, config, ...}:
+let cfg = config.networking;
+in
 {
   services.nscd.enable  = false;
   system.nssModules = lib.mkForce [];
@@ -8,7 +10,10 @@
   networking.hosts."192.168.0.167" = [ "t1.lilasp" ];
   networking.hosts."192.168.0.72"  = [ "bo.lilasp" ];
   networking.hosts."192.168.0.73"  = [ "mi.lilasp" ];
-  networking.hostFiles = [
-    "/home/hugosenari/.local/share/hosts"
-  ];
+  environment.etc.hosts.source = lib.mkDefault null;
+  environment.etc."hosts.d/system".source = pkgs.concatText "hosts" cfg.hostFiles;
+  system.activationScripts.hosts.text =
+    ''
+      cat /etc/hosts.d/* 2>/dev/null >/etc/hosts || true
+    '';
 }
