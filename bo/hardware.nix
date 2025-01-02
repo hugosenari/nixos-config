e72cfaafba79f8a5ebc4980e9f84fef6e99a346e
hugosenari <hugosenari@gmail.com>
2025-01-02 11:19:35 -0300

    bo: enable bluetooth

--- bo/hardware.nix
+++ bo/hardware.nix
@@ -16,12 +16,14 @@
 
   fileSystems."/".device  = "/dev/disk/by-uuid/03da4575-a0f0-4cf9-a280-d388cb20de91";
   fileSystems."/".fsType  = "ext4";
-
   fileSystems."/boot/efi".device = "/dev/disk/by-uuid/B8F1-3F27";
   fileSystems."/boot/efi".fsType = "vfat";
+
+
 
   swapDevices = [ { device = "/dev/disk/by-uuid/75f343d8-d50c-4c54-bc2b-f91b6bcb2fae"; } ];
 
   powerManagement.cpuFreqGovernor    = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.bluetooth.enable          = true;
 }
--- t1/hardware.nix
+++ t1/hardware.nix
@@ -3,22 +3,25 @@
 {
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];     
 
-  swapDevices = [ ];
-
   networking.hostName = "T1";
   networking.useDHCP  = lib.mkDefault true;
 
   boot.kernelModules  = [ "kvm-intel" ];
   boot.initrd.availableKernelModules   = [ "xhci_pci" "thunderbolt" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+
+
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint     = "/boot";
   boot.loader.systemd-boot.enable      = true;
+
   fileSystems."/".device               = "/dev/disk/by-label/nixos";
   fileSystems."/".fsType               = "ext4";
   fileSystems."/boot".device           = "/dev/disk/by-label/EFI";
   fileSystems."/boot".fsType           = "vfat";
   fileSystems."/home".device           = "/dev/disk/by-label/home";
   fileSystems."/home".fsType           = "ext4";
+
+  swapDevices = [ ];
 
   powerManagement.cpuFreqGovernor    = "powersave";
   hardware.cpu.intel.updateMicrocode = config.hardware.enableRedistributableFirmware;
