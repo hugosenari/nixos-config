9ac3f21ad20896ebd310965fc5ed8e73514dee04
hugosenari <hugosenari@gmail.com>
2025-01-22 00:39:59 -0300

    bo: enable virtualbox

--- bo/default.nix
+++ bo/default.nix
@@ -3,4 +3,7 @@
     ./hardware.nix
     ./synergy.nix
   ];
+
+  virtualisation.virtualbox.host.enable = true;
+  
 }
--- hugosenari/default.nix
+++ hugosenari/default.nix
@@ -3,7 +3,7 @@
   home-manager.useUserPackages        = true;
   home-manager.users.hugosenari       = inputs.self.homeModules.I;
   users.users.hugosenari.description  = "hugosenari";
-  users.users.hugosenari.extraGroups  = [ "networkmanager" "wheel" "sudo" "lp" "docker" ];
+  users.users.hugosenari.extraGroups  = [ "networkmanager" "wheel" "sudo" "lp" "docker" "vboxusers" ];
   users.users.hugosenari.isNormalUser = true;
   users.users.hugosenari.shell        = pkgs.fish;
   programs.fish.enable                = true;
