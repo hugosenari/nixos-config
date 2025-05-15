d77bd76380d354f4b66390f3c1bd0dd0aa01ef28
hugosenari <hugosenari@gmail.com>
2025-05-15 10:02:20 -0300

    remove hp host

--- flake.nix
+++ flake.nix
@@ -1,26 +1,20 @@
 {
   description     = "Hugosenari Hosts";
 
-  inputs.prev.url = "github:NixOS/nixpkgs/release-24.05";
-  inputs.next.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.curr.url = "github:NixOS/nixpkgs/release-24.11";
+  inputs.v2405.url = "github:NixOS/nixpkgs/release-24.05";
+  inputs.v2411.url = "github:NixOS/nixpkgs/release-24.11";
+  inputs.v2505.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.homa.url = "github:nix-community/home-manager/release-24.05";
-  inputs.homa.inputs.nixpkgs.follows = "prev";
+  inputs.homa.inputs.nixpkgs.follows = "v2405";
 
-  outputs = inputs: rec {
+  outputs = {v2405, v2411}@inputs: rec {
     nixosModules.ME.imports = [ ./hugosenari ];
     nixosModules.OS.imports = [ ./cfg.nix ./cache ./networking.nix ];
     nixosModules.BO.imports = [ nixosModules.OS nixosModules.ME ./bo ];
-    nixosModules.HP.imports = [ nixosModules.OS nixosModules.ME ./hp ];
     nixosModules.T1.imports = [ nixosModules.OS nixosModules.ME ./t1 ];
 
-    nixosConfigurations.BO  = lib.prevOS nixosModules.BO;
-    nixosConfigurations.HP  = lib.prevOS nixosModules.HP;
-    nixosConfigurations.T1  = lib.prevOS nixosModules.T1;
-
-    lib.prevOS = lib.os inputs.prev;
-    lib.currOS = lib.os inputs.curr;
-    lib.nextOS = lib.os inputs.next;
+    nixosConfigurations.BO  = lib.os v2405 nixosModules.BO;
+    nixosConfigurations.T1  = lib.os v2411 nixosModules.T0;
 
     lib.os = nixpkgs: cfg: nixpkgs.lib.nixosSystem {
       modules = [ inputs.homa.nixosModules.home-manager cfg ];
