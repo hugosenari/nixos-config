751471dd89e5d1783ed4838fef64c0222132b65d
hugosenari <hugosenari@gmail.com>
2025-05-15 09:31:03 -0300

    remove stand alone home-manager
    I used to have a Linux without NixOS
    Now all them are NixOS

--- flake.nix
+++ flake.nix
@@ -4,18 +4,15 @@
   inputs.prev.url = "github:NixOS/nixpkgs/release-24.05";
   inputs.next.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.curr.url = "github:NixOS/nixpkgs/release-24.11";
-  inputs.hm.url   = "github:nix-community/home-manager/release-24.05";
-  inputs.hm.inputs.nixpkgs.follows = "prev";
+  inputs.homa.url = "github:nix-community/home-manager/release-24.05";
+  inputs.homa.inputs.nixpkgs.follows = "prev";
 
   outputs = inputs: rec {
-    homeModules.I.imports   = [ ./hugosenari/home-manager.nix ];
-    homeConfigurations.I    = lib.home homeModules.I;
-
-    nixosModules.I.imports  = [ ./hugosenari ];
-    nixosModules.os.imports = [ ./cfg.nix ./cache ./networking.nix ];
-    nixosModules.BO.imports = [ nixosModules.os nixosModules.I ./bo ];
-    nixosModules.HP.imports = [ nixosModules.os nixosModules.I ./hp ];
-    nixosModules.T1.imports = [ nixosModules.os nixosModules.I ./t1 ];
+    nixosModules.ME.imports = [ ./hugosenari ];
+    nixosModules.OS.imports = [ ./cfg.nix ./cache ./networking.nix ];
+    nixosModules.BO.imports = [ nixosModules.OS nixosModules.ME ./bo ];
+    nixosModules.HP.imports = [ nixosModules.OS nixosModules.ME ./hp ];
+    nixosModules.T1.imports = [ nixosModules.OS nixosModules.ME ./t1 ];
 
     nixosConfigurations.BO  = lib.prevOS nixosModules.BO;
     nixosConfigurations.HP  = lib.prevOS nixosModules.HP;
@@ -24,16 +21,11 @@
     lib.prevOS = lib.os inputs.prev;
     lib.currOS = lib.os inputs.curr;
     lib.nextOS = lib.os inputs.next;
+
     lib.os = nixpkgs: cfg: nixpkgs.lib.nixosSystem {
-      modules = [ inputs.hm.nixosModules.home-manager cfg ];
+      modules = [ inputs.homa.nixosModules.home-manager cfg ];
       system  = "x86_64-linux";
       specialArgs.inputs = inputs // { nixpkgs = nixpkgs; };
-    };
-
-    lib.home = nixpkgs: cfg: inputs.hm.lib.homeManagerConfiguration {
-      modules = [ cfg ];
-      pkgs    = nixpkgs.legacyPackages.x86_64-linux;
-      extraSpecialArgs.inputs = inputs // { nixpkgs = nixpkgs; };
     };
   };
 }
