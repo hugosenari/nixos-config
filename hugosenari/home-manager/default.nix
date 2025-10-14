3cca788cc482d7c03dcee518a6efcfd24a91459a
hugosenari <hugosenari@gmail.com>
2025-10-13 23:50:32 -0300

    add hp back

--- hugosenari/home-manager/default.nix
+++ hugosenari/home-manager/default.nix
@@ -32,23 +32,4 @@ in
   programs.ssh.compression           = true;
   services.kbfs.enable               = true;
   services.keybase.enable            = true;
-  systemd.user.services.kbfs.Service.PrivateTmp = lib.mkForce false; # KBFS fail with true
-  systemd.user.services.id-ecdsa-signer.serviceConfig.TimeoutStopSec = "90";
-  systemd.user.services.id-ecdsa-signer.enable      = true;
-  systemd.user.services.id-ecdsa-signer.description = ''Resign id_ecdsa.pub to keep it fresh'';
-  systemd.user.services.id-ecdsa-signer.wantedBy    = [ "multi-user.target" ];
-  systemd.user.services.id-ecdsa-signer.serviceConfig.Type = "oneshot";
-  systemd.user.services.id-ecdsa-signer.script = ''
-    export LIBTMP2_PATH=/run/current-system/sw/lib/libtpm2_pkcs11.so
-    export PATH=${pkgs.openssh}/bin:$PATH
-    export SSH_ASKPASS=/home/hugosenari/.ssh/ask_p
-    ssh-keygen \
-      -s <(ssh-keygen -D $LIBTMP2_PATH) \
-      -D $LIBTMP2_PATH \
-      -V -2h:+2h \
-      -I "$(hostname)" \
-      -n $USER \
-      -z 1 \
-      /home/hugosenari/.ssh/id_ecdsa.pub
-  '';
 }
