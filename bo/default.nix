b000e2d27030343cf413f41fd932be3ca2340c36
hugosenari <hugosenari@gmail.com>
2025-11-01 22:41:54 -0300

    ssh-serve

--- bo/default.nix
+++ bo/default.nix
@@ -3,5 +3,5 @@
     ./hardware.nix
     ./synergy.nix
   ];
-  nix.settings.substituters = [ "http://hp.ka.gy:5000" ];
+  nix.settings.substituters = [ "ssh-ng://nix-ssh@hp.ka.gy?trusted=true&want-mass-query=true" ];
 }
