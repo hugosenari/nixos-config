eb185560e1db9cfcaf7e16c8acdd45e6dc23bece
hribeiro <Hugo.Ribeiro@AlticeUSA.com>
2025-12-01 08:51:56 -0300

    disabling remote builder

--- nix.desktop.nix
+++ nix.desktop.nix
@@ -1,13 +1,16 @@
-{
-  nix.distributedBuilds = true;
-  nix.buildMachines     = [{
+let
+  hp = {
     system   = "x86_64-linux";
     sshUser  = "nix-ssh";
-    sshKey   = "/home/hugosenari/.ssh/id_ecdsa-cert.pub";
+    sshKey   = "/ect/hugosenari/.ssh/id_ecdsa-cert.pub";
     protocol = "ssh-ng";
     maxJobs  = 100;
     hostName = "hp.ka.gy";
     publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUVBLzhNZDM1OC9lWG1ZUW9ZeUhScjlMa3lCdjFSZ1FTYkgyWXVmMFl0bHEgcm9vdEBIUAo=";
-  }];
-  nix.settings.substituters = [ "ssh-ng://nix-ssh@hp.ka.gy?trusted=true&want-mass-query=true" ];
+  };
+in
+{
+  # nix.distributedBuilds = true;
+  # nix.buildMachines     = [hp];
+  # nix.settings.substituters = [ "ssh-ng://nix-ssh@hp.ka.gy?trusted=true&want-mass-query=true" ];
 }
